1cfeac79c11745533251898c94ecbe32740c8f97
Bob Lannon <blannon@gmail.com>
2012-06-13 17:50:01 -0400

    first commit, started knowledge base app, set up fts, started knowledge_base test stubs

---
+++ knowledge_base/tests.py
@@ -0,0 +1,14 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+from knowledge_base import *
+
+class AdModelTest(TestCase):
+    def test_creating_a_new_Ad_and_saving_it_to_the_database(self):
+        # TODO: Create a new Ad object 
+        self.fail('todo: finish '+self.id())
