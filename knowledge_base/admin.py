23a56ba873dc48a64164a3ecda5e94c3612cfa42
Bob Lannon <blannon@gmail.com>
2012-06-19 16:42:27 -0400

    added inline Media and AdToCandidate objects to Ad, changed markets to filter_horizontal

--- knowledge_base/admin.py
+++ knowledge_base/admin.py
@@ -1,4 +1,36 @@
-from knowledge_base.models import Author
+from knowledge_base.models import Ad,AdToCandidate,Market,BroadcastType,Candidate,Funder,Coverage,Media
 from django.contrib import admin
 
-admin.site.register(Author)
+class MediaInline(admin.StackedInline):
+    model = Media
+    template = 'admin/stacked-media.html'
+    extra = 0
+
+class AdToCandidateInline(admin.StackedInline):
+    model = AdToCandidate
+    extra = 0
+
+class AdAdmin(admin.ModelAdmin):
+    inlines = [
+            MediaInline,
+            AdToCandidateInline
+            ]
+    filter_horizontal = [
+            'markets'
+            ]
+
+class CandidateAdmin(admin.ModelAdmin):
+    inlines = [
+            AdToCandidateInline
+            ]
+
+
+
+
+admin.site.register(Ad,AdAdmin)
+admin.site.register(Candidate,CandidateAdmin)
+admin.site.register(AdToCandidate)
+admin.site.register(Funder)
+admin.site.register(Coverage)
+admin.site.register(Market)
+admin.site.register(BroadcastType)
