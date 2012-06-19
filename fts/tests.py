71f4abb21494d3e9ad36b914a83ddff1c1daa3c9
Bob Lannon <blannon@gmail.com>
2012-06-19 11:38:47 -0400

    no longer inheriting from logintest, login fuct is separately defined

--- fts/tests.py
+++ fts/tests.py
@@ -5,9 +5,23 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+import time
+
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+def log_in_to_admin_site(lstc):
+    # open web browser, go to admin page
+    lstc.browser.get(lstc.live_server_url + '/admin/')
+    # see 'Django administration' heading
+    body = lstc.browser.find_element_by_tag_name('body')
+    # type username and password and hit enter
+    username_field = lstc.browser.find_element_by_name('username')
+    username_field.send_keys('blannon')
+    password_field = lstc.browser.find_element_by_name('password')
+    password_field.send_keys('cuibono')
+    password_field.send_keys(Keys.RETURN)
 
 class LoginTest(LiveServerTestCase):
     fixtures = ['admin_user.json']
@@ -39,9 +53,25 @@ class LoginTest(LiveServerTestCase):
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Site administration', body.text)
 
-class AuthorTest(LoginTest):
-    def test_can_create_new_author_via_admin_site(self):
-        # see hyperlinks that say "Authors"
-        self.fail('finish this test')
+class AuthorTest(LiveServerTestCase):
+    fixtures = ['admin_user.json']
 
-        
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_create_new_author_via_admin_site(self):
+        # login
+        log_in_to_admin_site(self)
+
+        # see hyperlinks that say "Authors"
+        authors_links = self.browser.find_elements_by_link_text('Authors')
+        #time.sleep(10)
+        #body = self.browser.find_element_by_tag_name('body')
+        #print body.text
+        self.assertEquals(len(authors_links), 1)
+
+        # TODO: use the admin site to create a new Author
+        self.fail('finish this test')
