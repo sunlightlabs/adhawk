59d0949430316db8f0cdbf3c2c6a85140244b5f2
Bob Lannon <blannon@gmail.com>
2012-07-18 17:21:08 -0400

    management command to upload rmse stats

---
+++ db_script/management/commands/upload_stats.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from db_script.stats_uploader import RMSEUploader
+from knowledge_base.models import Media
+
+from db_script.log import set_up_logger
+
+class Command(BaseCommand):
+
+    @transaction.commit_on_success
+    def handle(self, *args, **options):
+
+        if args:
+            data_dir = sys.argv[1]
+        else:
+            data_dir = 'db_script/processing'
+
+        log = set_up_logger("","db_script/processing")
+
+        rmse = RMSEUploader()
+        rmse.upload()
