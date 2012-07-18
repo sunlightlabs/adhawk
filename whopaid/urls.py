1952a6408d483d2ce8b973732cdd9d8070060b21
Bob Lannon <blannon@gmail.com>
2012-07-18 19:50:58 -0400

    directed ad/ to knowledge_base.urls

--- whopaid/urls.py
+++ whopaid/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin: 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/',include('whopaid_api.urls')),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root':
-        settings.MEDIA_ROOT})
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+    url(r'^ad/',include('knowledge_base.urls')),
 )
