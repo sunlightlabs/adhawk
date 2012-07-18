c155b6ce97f1578210b2027d0a0d56104e47c379
Bob Lannon <blannon@gmail.com>
2012-07-18 16:07:35 -0400

    allowed media_root to be changed in local_settings

--- whopaid/settings.py
+++ whopaid/settings.py
@@ -43,7 +43,7 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(SITE_ROOT,'media')
+#MEDIA_ROOT = os.path.join(SITE_ROOT,'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
