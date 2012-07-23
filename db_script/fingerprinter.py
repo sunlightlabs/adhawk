8df4863a7f238828b84acab977445a988eedcf43
Bob Lannon <blannon@gmail.com>
2012-07-23 15:34:49 -0400

    added commands for ingesting fingerprints

---
+++ db_script/fingerprinter.py
@@ -0,0 +1,59 @@
+import os
+import subprocess
+import json
+
+from knowledge_base.models import Media
+from whopaid.settings import MEDIA_ROOT
+from echoprint_server_api import fp
+
+VIDEO_DIR = os.path.join(os.path.abspath(MEDIA_ROOT),'videos')
+CODEGEN_DIR = os.path.join(os.path.abspath(MEDIA_ROOT),'codegens')
+
+class Fingerprinter():
+    def __init__(self,loc):
+        self.loc = loc
+        self.video_fname = self.get_video_fname()
+        self.codegen_fname = self.get_codegen_fname()
+        self.pk = self.get_pk()
+        self.media = self.get_media()
+    def get_codegen_fname(self):
+        return '.'.join([os.path.splitext(self.video_fname)[0],'cgn'])
+    def get_media(self):
+        return Media.objects.get(pk=self.pk)
+    def get_pk(self):
+        return int(self.video_fname.split('_')[1])
+    def get_video_fname(self):
+        return os.path.basename(self.loc)
+    def write_codegen_file(self,j):
+        fout = open(os.path.join(CODEGEN_DIR,self.codegen_fname),'w')
+        fout.write(str(j))
+        fout.close()
+    def build_ingest_dict(self,j):
+        i = {   "track_id"  : self.pk,
+                "fp"        : fp.decode_code_string(j['code']),
+                "codever"   : j['metadata']['version'],
+                "length"    : self.media.duration }
+        return i
+    def ingest_fingerprint(self,j):
+        i = self.build_ingest_dict(j)
+        print "ingesting %s"%(str(i))
+        fp.ingest(i)
+    def get_codegen(self):
+        process = subprocess.Popen(
+            ['echoprint-codegen',self.loc],
+            shell=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        exit_code = process.wait()
+        out,err = process.communicate()
+        if exit_code == 0:
+            j = json.loads(out)[0]
+            self.write_codegen_file(j)
+            self.ingest_fingerprint(j)
+            self.media.ingested = True
+            self.media.save()
+            return 'Media %s ingested as %s'%(str(self.pk).zfill(5),
+                                                self.codegen_fname)
+        else:
+            return 'Media %s ERROR %s'%(str(self.pk).zfill(5),
+                                            err)
---
+++ db_script/management/commands/ingest_fingerprints.py
@@ -0,0 +1,34 @@
+import os
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from db_script.fingerprinter import Fingerprinter
+from db_script.log import set_up_logger
+from whopaid.settings import MEDIA_ROOT
+from knowledge_base.models import Media
+
+VIDEO_DIR = os.path.join(MEDIA_ROOT,'videos')
+
+class Command(BaseCommand):
+
+    @transaction.commit_on_success
+    def handle(self, *args, **options):
+
+        log = set_up_logger('fingerprinter','db_script/processing')
+
+        w = os.walk(VIDEO_DIR)
+        medias = Media.objects.filter(ingested=False)
+        pks = [m.pk for m in medias]
+        dirpath, dirnames, fnames = w.next()
+        loc_dic = {}
+        for fn in fnames:
+            pk = int(fn.split('_')[1])
+            if pk in pks:
+                loc_dic[pk] = os.path.join(dirpath,fn)
+        if loc_dic:
+            for media_pk,loc in loc_dic.items():
+                f = Fingerprinter(loc)
+                log.info(f.get_codegen())
+        else:
+            print "Nothing to ingest"
