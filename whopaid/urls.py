7acb6ffe672b913f39f817a897c80589822528d1
Bob Lannon <blannon@gmail.com>
2012-08-07 16:54:38 -0400

    changed url for y u no match

--- whopaid/urls.py
+++ whopaid/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin: 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/',include('whopaid_api.urls')),
-    url(r'^no_match/','whopaid.views.no_match'),
+    url(r'^troubleshooting/','whopaid.views.no_match'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT}),
     url(r'^about/$','whopaid.views.about'),
