3cc9e847454f1609d68096955c074cbb3a38c379
Bob Lannon <blannon@gmail.com>
2012-08-13 16:29:53 -0400

    added count to report_ad_media

--- db_script/management/commands/report_ad_media.py
+++ db_script/management/commands/report_ad_media.py
@@ -1,7 +1,10 @@
 from django.core.management.base import BaseCommand
 
 from db_script.ad_media_reporter import AdMediaReporter
+from knowledge_base.models import Media
 
 class Command(BaseCommand):
+    count = Media.objects.filter(checked=False)
+
     def handle(self, *args, **options):
-        amr = AdMediaReporter()
+        amr = AdMediaReporter(count)
