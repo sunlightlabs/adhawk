eae26d7de31c50fbd8972e902879dbe1a00c6f52
Bob Lannon <blannon@gmail.com>
2012-08-17 03:09:29 -0400

    added search app for custom view and urlconf

---
+++ search/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+
+from haystack.forms import FacetedSearchForm
+from haystack.query import SearchQuerySet
+from haystack.views import FacetedSearchView
+
+sqs =SearchQuerySet().facet(
+                      'funder_name'
+                    ).facet(
+                      'funder_party'
+                    ).facet(
+                      'funder_committee_type'
+                    )
+
+urlpatterns = patterns('haystack.views',
+        url(r'^$', FacetedSearchView(form_class=FacetedSearchForm,
+                                        template='search/search.html',
+                                        searchqueryset=sqs),
+                                        name='haystack_search'),
+        )
