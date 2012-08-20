447038310c7072cba4609a69add0f6301b629eb9
Bob Lannon <blannon@gmail.com>
2012-08-20 00:49:39 -0400

    added script for interactive funder family identification

---
+++ db_script/family_finder.py
@@ -0,0 +1,56 @@
+import os
+from itertools import combinations
+
+from nltk import wordpunct_tokenize
+from nltk.metrics.distance import jaccard_distance,edit_distance
+
+from whopaid.settings import SITE_ROOT
+PROC_DIR = os.path.join(SITE_ROOT,'db_script/processing/family_finder')
+
+from knowledge_base.models import Funder
+
+STOPS = [line.strip() for line in 
+            open(os.path.join(PROC_DIR,'stopwords.txt')).readlines()]
+
+class FunderLevenshteinComparison():
+    def __init__(self,combo):
+        self.f1,self.f2 = combo
+        self.distance = edit_distance(
+                self.clean_name(self.f1.name),
+                self.clean_name(self.f2.name))
+        #self.distance = edit_distance(
+        #        self.clean_street(self.f1),
+        #        self.clean_street(self.f2))
+    def clean_name(self,name):
+        tokens = wordpunct_tokenize(name)
+        return ' '.join(
+                [t for t in tokens])
+    def clean_street(self,f):
+        st = ''
+        if f.street_one:
+            st += f.street_one
+        if f.street_two:
+            st += f.street_two
+        return st
+        
+class FunderJaccardComparison():
+    def __init__(self,combo):
+        self.f1,self.f2 = combo
+        self.f1_set = set(self.clean_name(f1.name))
+        self.f2_set = set(self.clean_name(f2.name))
+        self.distance = jaccard_distance(self.f1_set,self.f2_set)
+        self.same_state = self.compare_state()
+    def clean_name (self,name):
+        tokens = wordpunct_tokenize(name)
+        return [t for t in tokens if t.isalnum() and t not in STOPS]
+
+class FunderIdentityComparison():
+    def __init__(self,combo):
+        self.f1,self.f2 = combo
+        self.distance = self.identity()
+        self.same_state = self.compare_state()
+    def identity(self):
+        if self.f1.name == self.f2.name:
+            return 0
+        else:
+            return 1
---
+++ db_script/management/commands/find_family.py
@@ -0,0 +1,144 @@
+import os
+import itertools
+
+from django.core.management import BaseCommand
+from django.db import transaction
+
+from db_script.family_finder import FunderLevenshteinComparison, \
+                                    FunderJaccardComparison, \
+                                    FunderIdentityComparison
+from whopaid.settings import SITE_ROOT
+from knowledge_base.models import Funder,MediaProfile,FunderFamily
+
+PROC_DIR = os.path.join(SITE_ROOT,'db_script/processing/family_finder')
+STATES = [line.strip() for line in 
+                open(os.path.join(PROC_DIR,'states.txt')).readlines()]
+
+class Command(BaseCommand):
+
+    def print_funder_info(self,f):
+        print f.name
+        print f.committee_type
+        print f.street_one,f.street_two
+        print f.city+',',f.state
+
+
+    def get_confirmation(self,combo,distance):
+        funder_prime,other_funder = combo
+        divider = "="*80
+        print divider
+        print "Method: %s\tDistance:%f"%(self.method,distance)
+        print divider
+        print "Funder:"
+        self.print_funder_info(funder_prime)
+        print divider
+        print "Other Funder:"
+        self.print_funder_info(other_funder)
+        print "\n"
+        confirm = raw_input("Are these two the same? (y/n)")
+        if confirm.lower()[0] == "y":
+            other_funder.funder_family = funder_prime.funder_family
+            other_funder.save()
+            print "added",other_funder,"to",other_funder.funder_family
+            return other_funder
+        else:
+            print "okay, no dice"
+            return False
+
+    def compare_funders(self):
+        c = self.comparison(self.current_combo)
+        if c.distance >= self.threshold:
+            print c.distance
+            return False
+        else:
+            print "compared using %s with threshold %f (score %f)"%(
+                self.method,self.threshold,c.distance)
+            return self.get_confirmation(self.current_combo,c.distance)
+
+    def new_combo_list(self):
+        return itertools.product(
+                    self.funders_with_mp,self.other_funders)
+    
+    def get_funders_with_mp(self,state):
+        return [mp.funder for mp in MediaProfile.objects.all() 
+                if mp.funder.state == state]
+
+    def get_other_funders(self,state):
+        funder_set = set()
+        for f in Funder.objects.filter(state=state):
+            if f.funder_family:
+                if len(f.funder_family.funder_set.all()) <= 1:
+                    funder_set.add(f)
+                else:
+                    continue
+            else:
+                print f,"had no funder family?"
+                funder_set.add(f)
+        other_funder_set = funder_set - set(self.funders_with_mp)
+        return list(other_funder_set)
+
+    @transaction.commit_on_success
+    def handle(self, *args, **options):
+
+        if args:
+            self.method = args[0]
+            self.threshold = float(args[1])
+        else:
+            self.method = None
+            self.threshold = 1
+
+        if self.method == 'levenshtein':
+            self.comparison = FunderLevenshteinComparison
+        elif self.method == 'jaccard':
+            self.comparison = FunderJaccardComparison
+        else:
+            self.comparison = FunderIdentityComparison
+        
+        self.remove_set = set()
+
+        for state in STATES:
+            self.funders_with_mp = self.get_funders_with_mp(state)
+            self.other_funders = self.get_other_funders(state)
+
+            self.fc = self.new_combo_list()
+
+            keep_going = True
+
+            while keep_going:
+                try:
+                    self.current_combo = self.fc.next()
+                except StopIteration:
+                    print "Finished %s"%(state)
+                    keep_going = False
+                    continue
+                if self.current_combo in self.remove_set:
+                    continue
+                else:
+                    other_funder = self.compare_funders()
+                    if other_funder:
+                        self.other_funders.remove(other_funder)
+                        self.fc = self.new_combo_list()
+                        kg = raw_input("Keep going? (y/n)")
+                        if kg.lower()[0] == 'y':
+                            continue
+                        else:
+                            keep_going = False
+                            continue
+                    else:
+                        self.remove_set.add(self.current_combo)
+                        continue
+            print "populating mediaprofile input fields"
+            for fwmp in self.funders_with_mp:
+                mp_url = fwmp.mediaprofile_set.all()[0].url
+                ff = fwmp.funder_family
+                for f in ff.funder_set.all():
+                    f.media_profile_url_input = mp_url
+                    f.save()
+
+        print "cleaning up..."
+        for ff in FunderFamily.objects.all():
+            if ff.funder_set.all():
+                continue
+            else:
+                print "deleting orphan:",ff
+                ff.delete()
