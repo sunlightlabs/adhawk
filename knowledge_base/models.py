0b8034ce1390a3ab5c76e58a3ad1c514d084da23
Bob Lannon <blannon@gmail.com>
2012-08-30 14:04:32 -0400

    added gigya_url request function and a method for calling it on the Media object

--- knowledge_base/models.py
+++ knowledge_base/models.py
@@ -1,44 +1,42 @@
 import urlparse,httplib
 from decimal import Decimal
+import requests
+import json
 
 from django.db import models
 from django.db.models import Sum
 from django.template.defaultfilters import slugify
 
 from whopaid.settings import EXTERNAL_URL,MEDIA_URL
+from whopaid.local_settings import GIGYA_ENDPOINT,GIGYA_API_KEY,GIGYA_SECRET
 
 def slugify_uniquely(value, model, slugfield="slug"):
-        """Returns a slug on a name which is unique within a model's table
-
-        This code suffers a race condition between when a unique
-        slug is determined and when the object with that slug is saved.
-        It's also not exactly database friendly if there is a high
-        likelyhood of common slugs being attempted.
-
-        A good usage pattern for this code would be to add a custom save()
-        method to a model with a slug field along the lines of:
-
-                from django.template.defaultfilters import slugify
-
-                def save(self):
-                    if not self.id:
-                        # replace self.name with your prepopulate_from field
-                        self.slug = SlugifyUniquely(self.name, self.__class__)
-                super(self.__class__, self).save()
-
-        Original pattern discussed at
-        http://www.b-list.org/weblog/2006/11/02/django-tips-auto-populated-fields
-        """
-        suffix = 0
-        potential = base = slugify(value)
-        while True:
-                if suffix:
-                        potential = "-".join([base, str(suffix)])
+    suffix = 0
+    potential = base = slugify(value)
+    while True:
+       if suffix:
+           potential = "-".join([base, str(suffix)])
                 
-                if not model.objects.filter(**{slugfield: potential}).count():
-                        return potential
-                # we hit a conflicting slug, so bump the suffix & try again
-                suffix += 1
+       if not model.objects.filter(**{slugfield: potential}).count():
+           return potential
+       # we hit a conflicting slug, so bump the suffix & try again
+       suffix += 1
+
+def gigya_url_request(media_object):
+    base_url = 'http://adhawk.sunlightfoundation.com/ad/'
+    url = base_url+media_object.slug
+    cid = '[Ad Hawk] /ad adprofile %d_%d'%(media_object.media_profile_id,media_object.pk)
+    payload = { 'apiKey'            :  GIGYA_API_KEY,
+                'secret'            :  GIGYA_SECRET,
+                'url'               :  url,
+                'cid'               :  cid,
+                'format'            : 'json',
+                'httpStatusCodes'   : 'false'}
+    d = requests.get(GIGYA_ENDPOINT,params=payload).json
+    if d['statusCode'] == 200:
+        return d['shortURL']
+    else:
+        return None
 
 # Create your models here.
 class Author(models.Model):
@@ -598,6 +596,11 @@ class Media(models.Model):
     # tags = models.ManyToManyField(Tag)
     def __unicode__(self):
         return "%s (%s)"%(self.ad.title,self.url)
+
+    def request_gigya_url(self):
+        self.gigya_url = gigya_url_request(self)
+        #print self.gigya_url
+        self.save()
     
     def save(self, *args, **kwargs):
         self.funder_name = self.media_profile.funder.name
