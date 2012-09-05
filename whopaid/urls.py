6c5749293963b374dd9490cfb8588da04d491722
Bob Lannon <blannon@gmail.com>
2012-09-05 11:23:50 -0400

    starting sitemap addition

--- whopaid/urls.py
+++ whopaid/urls.py
@@ -16,16 +16,16 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^$','whopaid.views.landing_page'),
+    url(r'^$','whopaid.views.landing_page',name='static_landing'),
     url(r'^400/','django.views.defaults.page_not_found'),
     url(r'^500/','django.views.defaults.server_error'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/',include('whopaid_api.urls')),
-    url(r'^troubleshooting/','whopaid.views.no_match'),
+    url(r'^troubleshooting/','whopaid.views.no_match',name='static_no_match'),
     url(r'^mailinglist/', include('spammer.urls')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT}),
-    url(r'^about/$','whopaid.views.about'),
+    url(r'^about/$','whopaid.views.about',name='static_about'),
     url(r'^glossary/$','whopaid.views.glossary',name='glossary'),
     url(r'^ad/top/$','knowledge_base.views.top_ads'),
     url(r'^ad/top/(?P<path>.*)/$','knowledge_base.views.top_ad_select'),
@@ -33,3 +33,14 @@ urlpatterns = patterns('',
     url(r'^sponsor/(?P<path>.*)/$', 'knowledge_base.views.funder_family_profile'),
     url(r'^search/', include('search.urls')),
     )
+
+my_staticsites = (('/','monthly','whopaid/landing_page.html'),
+               ('about/','monthly','whopaid/about.html'),
+               ('glossary/','monthly','whopaid/glossary.html'),
+               ('ad/top/','daily','knowledge_base/top_ads.html'),)
+
+sitemaps = {
+        'ad_profiles': AdProfileSiteMap,
+        'funder_family_profiles': FunderFamilyProfileSiteMap,
+        'static':StaticSitemap(my_staticsites)
+        }
