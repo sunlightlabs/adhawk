2deb3992a090145c5fb875f5bfc3e5c0393dd213
Bob Lannon <blannon@sunlightfoundation.com>
2012-09-07 20:50:23 +0000

    added ranking job to twice_daily

--- cronjobs/adhawk.twice_daily.sh
+++ cronjobs/adhawk.twice_daily.sh
@@ -6,6 +6,7 @@ MY_PYTHON=/home/blannon/.virtualenvs/whopaid/bin/python
 DJANGO_PROJECT=/home/blannon/whopaid
 
 unbuffer $MY_PYTHON $DJANGO_PROJECT/manage.py import_ftum
+unbuffer $MY_PYTHON $DJANGO_PROJECT/manage.py rank_top_ads
 
 echo "------------ end adhawk.twice_daily.sh $(date --rfc-3339=seconds) -----------"
 )
