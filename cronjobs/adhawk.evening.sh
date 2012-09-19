443953087d0666d3057df401844eb1f567f4adc0
Bob Lannon <blannon@gmail.com>
2012-09-18 22:16:10 -0400

    added update_index to adhawk.evening

--- cronjobs/adhawk.evening.sh
+++ cronjobs/adhawk.evening.sh
@@ -7,6 +7,7 @@ DJANGO_PROJECT=/home/blannon/whopaid
 
 $MY_PYTHON $DJANGO_PROJECT/manage.py download_videos
 $MY_PYTHON $DJANGO_PROJECT/manage.py ingest_fingerprints
+$MY_PYTHON $DJANGO_PROJECT/manage.py update_index
 
 echo "------- end adhawk.evening.sh  $(date --rfc-3339=seconds) -------------------"
 )
