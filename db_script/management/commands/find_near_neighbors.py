5f253509d6ae7b7029d1adfff06091ff5de87a12
Bob Lannon <blannon@gmail.com>
2012-09-25 01:05:22 -0400

    changed new management commands to implement BaseCommand

--- db_script/management/commands/find_near_neighbors.py
+++ db_script/management/commands/find_near_neighbors.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
 from superfastmatch import client
 
 from knowledge_base.models import Media,MediaNearNeighbor
@@ -7,27 +10,30 @@ from knowledge_base.models import Media,MediaNearNeighbor
 
 sfm_client = client.Client()
 
-out = open('sfm_results.csv','w')
+#out = open('sfm_results.csv','w')
 
-for mmn in MediaNearNeighbor.objects.all():
-    mmn.delete()
+class Command(BaseCommand):
 
-for m in Media.objects.filter(ingested=True):
-    doc = sfm_client.get(1,m.id)
-    comp_list = []
-    for other_doc in doc['documents']['rows']:
-        shared = 0
-        for frag in other_doc['fragments']:
-            shared += frag[2]
-        comp_list.append((other_doc['docid'],shared))
-        #out.write('\t'.join([str(a) for a in [m.id,other_doc['docid'],shared]]))
-        #out.write('\n')
-    comp_list = sorted(comp_list,key=lambda x: x[1],reverse=True)[0:10]
-    l = min([len(comp_list),10])
-    for i in range(l):
-        od,s = comp_list[i]
-        m2 = Media.objects.get(id=od)
-        mnn = MediaNearNeighbor(media=m,neighbor=m2,rank=i+1)
-        mnn.save()
+    def handle(self, *args, **options):
+        for mmn in MediaNearNeighbor.objects.all():
+            mmn.delete()
 
-#out.close()
+        for m in Media.objects.filter(ingested=True):
+            doc = sfm_client.get(1,m.id)
+            comp_list = []
+            for other_doc in doc['documents']['rows']:
+                shared = 0
+                for frag in other_doc['fragments']:
+                    shared += frag[2]
+                comp_list.append((other_doc['docid'],shared))
+                #out.write('\t'.join([str(a) for a in [m.id,other_doc['docid'],shared]]))
+                #out.write('\n')
+            comp_list = sorted(comp_list,key=lambda x: x[1],reverse=True)[0:10]
+            l = min([len(comp_list),10])
+            for i in range(l):
+                od,s = comp_list[i]
+                m2 = Media.objects.get(id=od)
+                mnn = MediaNearNeighbor(media=m,neighbor=m2,rank=i+1)
+                mnn.save()
+
+        #out.close()
