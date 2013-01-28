0ae832bf6f73cdf2f26c61eb3308bcccdb67264d
Bob Lannon <blannon@gmail.com>
2013-01-28 11:32:52 -0500

    fixed typo

--- cronjobs/adhawk.weekly.sh
+++ cronjobs/adhawk.weekly.sh
@@ -7,7 +7,7 @@ DJANGO_PROJECT=/home/blannon/whopaid
 
 $MY_PYTHON $DJANGO_PROJECT/manage.py import_fec
 $MY_PYTHON $DJANGO_PROJECT/manage.py update_fec_kb
-$MY_PYTHON $DJANGO_PROJECT/manage.py initalize_funder_families
+$MY_PYTHON $DJANGO_PROJECT/manage.py initialize_funder_families
 
 echo "------------- ending adhawk.weekly.sh $(date --rfc-3339=seconds) ------------------"
 )
