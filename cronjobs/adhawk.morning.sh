1c7bcf93996b700cc723bb119e574c94c004b371
Bob Lannon <blannon@gmail.com>
2014-02-12 18:32:36 -0500

    quotes

--- cronjobs/adhawk.morning.sh
+++ cronjobs/adhawk.morning.sh
@@ -7,22 +7,22 @@ DJANGO_PROJECT=/home/blannon/whopaid
 
 unbuffer $MY_PYTHON $DJANGO_PROJECT/manage.py import_ad_media 2> $TMP_LOG
 if [ "$?" -eq 1 ]; then
-    logger -t "import_ad_media" $(cat $TMP_LOG)
+    logger -t "import_ad_media" "$(cat $TMP_LOG)"
 fi
 
 unbuffer $MY_PYTHON $DJANGO_PROJECT/manage.py get_thumbs 2> $TMP_LOG
 if [ "$?" -eq 1 ]; then
-    logger -t "get_thumbs" $(cat $TMP_LOG)
+    logger -t "get_thumbs" "$(cat $TMP_LOG)"
 fi
 
 unbuffer /home/blannon/make_strips.sh 2> $TMP_LOG
 if [ "$?" -eq 1 ]; then
-    logger -t "make_strips" $(cat $TMP_LOG)
+    logger -t "make_strips" "$(cat $TMP_LOG)"
 fi
 
 $MY_PYTHON $DJANGO_PROJECT/manage.py report_ad_media 2> $TMP_LOG
 if [ "$?" -eq 1 ]; then
-    logger -t "report_ad_media" $(cat $TMP_LOG)
+    logger -t "report_ad_media" "$(cat $TMP_LOG)"
 fi
 
 logger -t "adhawk.morning" "---- end adhawk.morning.sh $(date --rfc-3339=seconds) -----"
